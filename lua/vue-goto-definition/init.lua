5b6c0b4a942b2423d0f6be310599a782813b812f
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-03-17 04:13:57 -0500

    feat: goto definition works for imports defined in auto-imports.d.ts, components.d.ts.  Filters loclist if definition is not local file.

--- lua/vue-goto-definition/init.lua
+++ lua/vue-goto-definition/init.lua
@@ -2,21 +2,50 @@ M = {}
 
 local lsp_definition = vim.lsp.buf.definition
 
-local function component_init()
-	vim.api.nvim_create_augroup("VueGotoDefinition", { clear = true })
+local ai_regx = ".*/auto%-imports%.d%.ts$"
+local cmp_regx = ".*/components%.d%.ts$"
+
+local function autocmd()
+	local group = vim.api.nvim_create_augroup("VueGotoDefinition", { clear = true })
 	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 		pattern = { "*.vue" },
+		group = group,
 		callback = function()
 			local on_list = {
 				on_list = function(list)
+					local found = false
 					if list and list.items then
 						for _, item in ipairs(list.items) do
-							if string.match(item.filename, ".*/components.d.ts$") then
-								local file = string.match(item.text, "import%('(.-)'%)")
-								if file then
-									vim.cmd.edit(file)
+							local import = string.match(item.text, "import%('(.-)'%)")
+							if import and string.match(import, "^%./") then
+								if item.filename:match(ai_regx) then
+									if not string.match(import, "%.ts$") then
+										import = import .. ".ts"
+									end
+									found = true
+								elseif item.filename:match(cmp_regx) then
+									found = true
 								end
-								break
+								if found then
+									vim.cmd.edit(import)
+									break
+								end
+							end
+						end
+					end
+					if not found then
+						local new_list = {}
+						for _, item in ipairs(list.items) do
+							if not item.filename:match(ai_regx) or item.filename:match(cmp_regx) then
+								table.insert(new_list, item)
+							end
+						end
+						if #new_list > 0 then
+							if #new_list == 1 then
+								vim.cmd.edit(new_list[1].filename)
+							else
+								vim.fn.setloclist(0, new_list)
+								vim.cmd.lopen()
 							end
 						end
 					end
@@ -35,7 +64,7 @@ end
 ---@diagnostic disable-next-line: duplicate-set-field
 function M.setup(config)
 	config = config or {}
-	component_init()
+	autocmd()
 end
 
 return M
