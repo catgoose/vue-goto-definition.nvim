4c0ef8f8d4ca893e77bfc3946b57969bb09291c9
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-03-17 21:21:55 -0500

    feat(config): adds framework detection configuration and provides patterns for each framework

--- lua/vue-goto-definition/config.lua
+++ lua/vue-goto-definition/config.lua
@@ -4,19 +4,57 @@ local _opts = {
 	auto_imports = true,
 	components = true,
 	filetypes = { "vue" },
-	enabled = function()
-		return vim.fn.filereadable("vite.config.ts") == 1
-	end,
+	detection = {
+		nuxt = function()
+			return vim.fn.glob(".nuxt/") ~= ""
+		end,
+		vue3 = function()
+			return vim.fn.filereadable("vite.config.ts") == 1
+		end,
+		priority = { "nuxt", "vue3" },
+	},
 }
 
-Config.init = function(opts)
+local framework = nil
+
+local import = [[import%(['|"](.-)['|"]%)]]
+local patterns = {
+	vue3 = {
+		auto_imports = ".*/auto%-imports%.d%.ts$",
+		components = ".*/components%.d%.ts$",
+		import = import,
+		import_prefix = "^%./",
+	},
+	nuxt = {
+		auto_imports = ".*/%.nuxt/types/imports%.d%.ts$",
+		components = ".*/%.nuxt/components%.d%.ts$",
+		import = import,
+		import_prefix = "^%.%./",
+	},
+}
+
+Config.set_opts = function(opts)
 	opts = opts or {}
 	_opts = vim.tbl_deep_extend("keep", opts, _opts)
+	for _, detection in ipairs(_opts.detection.priority) do
+		if _opts.detection[detection]() then
+			framework = detection
+			break
+		end
+	end
 	return Config.get_opts()
 end
 
 Config.get_opts = function()
 	return _opts
+end
+
+Config.get_framework = function()
+	return framework
+end
+
+Config.get_patterns = function()
+	return patterns
 end
 
 return Config
