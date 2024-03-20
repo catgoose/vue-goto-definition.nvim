004625dd841dd948c624e9c17cb533eb7f46adfb
Joshua Tye <21010072+catgoose@users.noreply.github.com>
2024-03-19 20:30:45 -0500

    ref: creates locationlist module

--- lua/vue-goto-definition/autocmd.lua
+++ lua/vue-goto-definition/autocmd.lua
@@ -1,32 +1,11 @@
 local config = require("vue-goto-definition.config")
 local import = require("vue-goto-definition.import")
 local utils = require("vue-goto-definition.utils")
+local locationlist = require("vue-goto-definition.locationlist")
 
 local M = {}
 
 local lsp_definition = vim.lsp.buf.definition
-
-local function filter_location_list(list, patterns)
-	local filter = config.get_opts().filter
-	return vim.tbl_filter(function(item)
-		local is_auto_import = filter.auto_imports and item.filename:match(patterns.auto_imports)
-		local is_component = filter.components and item.filename:match(patterns.components)
-		local is_same_file = filter.same_file and item.filename == vim.fn.expand("%:p")
-		return not is_auto_import and not is_component and not is_same_file
-	end, list.items or {})
-end
-
-local function open_location_list(items)
-	if #items > 0 then
-		if #items == 1 then
-			vim.cmd.edit(items[1].filename)
-			vim.api.nvim_win_set_cursor(0, { items[1].lnum, items[1].col - 1 })
-		else
-			vim.fn.setloclist(0, items)
-			vim.cmd.lopen()
-		end
-	end
-end
 
 local _items = {}
 
@@ -41,8 +20,7 @@ M.setup = function(framework, patterns)
 					if not list or not list.items or #list.items == 0 or not utils.vue_tsserver_plugin_loaded() then
 						return
 					end
-					local items = filter_location_list(list, patterns)
-					vim.list_extend(_items, items)
+					vim.list_extend(_items, locationlist.get_filtered_items(list, patterns))
 					vim.defer_fn(function()
 						if #_items == 0 then
 							return
@@ -51,7 +29,7 @@ M.setup = function(framework, patterns)
 						if found_import_path then
 							vim.cmd.edit(found_import_path)
 						else
-							open_location_list(_items)
+							locationlist.open(_items)
 						end
 					end, config.get_opts().defer)
 				end,
